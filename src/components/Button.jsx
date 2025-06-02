55478596512cb135b06cd0bba83b47399b662dff
sifatbro786 <solaimanislamsifat@gmail.com>
2025-06-02 22:38:59 +0600

    added a favIcon

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,7 +2,7 @@ import { IoIosReturnRight } from "react-icons/io";
 
 export default function Button({ children }) {
     return (
-        <button className="min-w-40 px-4 py-2 bg-zinc-100 text-black text-sm font-medium rounded-full flex items-center justify-between">
+        <button className="min-w-40 px-4 py-2 bg-zinc-100 text-black text-sm font-light rounded-full flex items-center justify-between">
             {children}
             <IoIosReturnRight />
         </button>
